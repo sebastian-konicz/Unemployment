ebc2abaec842624e10b579d020956b2726c88df4
Sebastian Konicz <sebastian.konicz@gmail.com>
2021-05-08 21:18:09 +0200

    Found a way to implement popups and tooltips but for now i will let i be like it is. Code is copied in other scripts files

--- src/02_map.py
+++ src/02_map.py
@@ -25,7 +25,6 @@ def main():
     map = map[['JPT_KOD_JE', 'geometry']]
     map['JPT_KOD_JE'] = map['JPT_KOD_JE'].apply(lambda x: str(x))
 
-    print(map.dtypes)
     # loading unemployment data
     data_path = r'\data\interim\unemployment.xlsx'
     data = pd.read_excel(project_dir + data_path)
@@ -39,29 +38,16 @@ def main():
     # merging dataframes
     map = map.merge(data, left_on='JPT_KOD_JE', right_on='teryt')
 
-    print(map.head(10))
-
     # simplifying geometry
     map.geometry = map.geometry.simplify(0.005)
 
     # changing data to GeoJSON
     map_geo = map.to_json()
 
-    # # prepare the customised text
-    # tooltip_text = []
-    # for idx in range(len(data)):
-    #     tooltip_text.append(str(data['pow_name'][idx]) + ' ' + str(data['unempl_%'][idx]) + '%')
-    # tooltip_text
-    #
-    # # append a tooltip column with customised text
-    # for idx in range(len(tooltip_text)):
-    #     print(map_geo)
-    #     map_geo['features'][idx]['properties']['tooltip'] = tooltip_text[idx]
-
     # creating folium map
     map_graph = folium.Map([52, 19], zoom_start=7)
 
-    choropleth = folium.Choropleth(geo_data=map_geo,
+    folium.Choropleth(geo_data=map_geo,
                       name='choropleth',
                       data=data,
                       columns=['teryt', 'unempl_%'],
@@ -73,73 +59,26 @@ def main():
                       highlight=True,
                       legend_name="Stopa bezrobocia w procentach").add_to(map_graph)
 
+    # adding labels to map
+    style_function = lambda x: {'fillColor': '#ffffff',
+                                'color': '#000000',
+                                'fillOpacity': 0.1,
+                                'weight': 0.1}
 
-    # # VINCENT
-    import json
-    import requests
-    url = ("https://raw.githubusercontent.com/python-visualization/folium/master/examples/data")
-    vis1 = json.loads(requests.get(f"{url}/vis1.json").text)
-    # print(vis1)
-    # vincent = folium.Vega(vis1, width=450, height=250)
-    # # add_child(vincent)
-    #
-    # html text in tooltip
-    # html = """
-    #         <b>dupa</b><br>
-    #         """
+    tooltip = folium.features.GeoJson(
+        map_geo,
+        style_function=style_function,
+        control=False,
+        tooltip=folium.features.GeoJsonTooltip(
+            fields=['pow_name', 'unempl_%'],
+            aliases=['nazwa', 'stopa bezrobocia (%)'],
+            style=("background-color: white; color: #333333; font-family: arial; font-size: 12px; padding: 10px;")
+        ),
+    )
 
-    html = folium.Vega(vis1, width=450, height=250)
-    print(folium.Vega(vis1, width=450, height=250))
-    print(type(folium.Vega(vis1, width=450, height=250)))
-
-    # html style
-    style = "background-color: white; " \
-                 "color: #333333; " \
-                 "font-family: arial; " \
-                 "font-size: 16px; " \
-                 "padding: 10px;"
-
-    # adding tooltip do map
-    folium.map.Tooltip(html, style=style, sticky=True).add_to(choropleth)
-
-    html = """
-        <h1> This is a big popup</h1><br>
-        With a few lines of code...
-        <p>
-        <code>
-            from numpy import *<br>
-            exp(-2*pi)
-        </code>
-        </p>
-        """
-    iframe = IFrame(html=html, width=500, height=300)
-    folium.map.Popup(iframe, max_width=2650).add_child(folium.Vega(vis1, width=450, height=250)).add_to(choropleth)
-
-    # popup=folium.features.GeoJsonPopup(folium.Popup(max_width=400).add_child(folium.VegaLite(vis1, width=400, height=300)))
-
-    # # adding labels to map
-    # style_function = lambda x: {'fillColor': '#ffffff',
-    #                             'color': '#000000',
-    #                             'fillOpacity': 0.1,
-    #                             'weight': 0.1}
-    #
-    # # choropleth.geojson.add_child(folium.features.GeoJsonTooltip(['pow_name'], labels=False))
-    #
-    # tooltip = folium.features.GeoJson(
-    #     map_geo,
-    #     style_function=style_function,
-    #     control=False,
-    #     tooltip=folium.features.GeoJsonTooltip(
-    #         fields=['pow_name', 'unempl_%'],
-    #         aliases=['nazwa', 'stopa bezrobocia (%)'],
-    #         style=("background-color: white; color: #333333; font-family: arial; font-size: 12px; padding: 10px;")
-    #     ),
-    #     popup=folium.features.GeoJsonPopup(['pow_name', 'unempl_%'])
-    # )
-    #
-    # map_graph.add_child(tooltip)
+    map_graph.add_child(tooltip)
     # map_graph.keep_in_front(tooltip)
-    # folium.LayerControl().add_to(map_graph)
+    folium.LayerControl().add_to(map_graph)
 
     # saving map
     print('saving map')
